3934ea808b9778126a076b2f53aaa97b6709d257
dougfraga <douglas.fraga@gmail.com>
2020-08-20 16:27:11 -0300

    Criado o atributo completo "filhos".

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,12 +1,15 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=36):
+    def __init__(self, nome=None, idade=36, *filhos):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá, meu nome é {self.nome} e tenho {self.idade} anos.'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Doug', 37)
-    print(p.cumprimentar())
+    doug = Pessoa('Doug', 37, 'Flor', 'Maju')
+    braaw = Pessoa('Nilton', 70, doug)
+    for filho in braaw.filhos:
+        print(filho.nome)
